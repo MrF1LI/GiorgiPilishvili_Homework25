02c3ebd20382896fb73a5f55e2e1ffa17fda60d9
Giorgi Pilishvili <giorgi.pilishvili.1@btu.edu.ge>
2022-08-23 12:56:27 +0400

    Initial Commit

---
+++ GiorgiPilishvili_Homework25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiorgiPilishvili_Homework25
+//
+//  Created by GIORGI PILISSHVILI on 23.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
